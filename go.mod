8b9f95eb0cd1ec40f59ca3fcc296afa951bbc523
Rakesh D R <rakeshdr543@gmail.com>
2023-06-25 11:24:10 +0530

    Added server setup

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/rakeshdr543/rss-aggregator
 
 go 1.18
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("No port defined")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"foo.com"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/health", handleReadiness)
+	v1Router.Get("/error", handleError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Starting server on port %s", portString)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
